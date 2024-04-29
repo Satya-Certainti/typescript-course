c0a8df9982d56d01485fef28dd898f318651f47d
Satya-Certainti <satyanarayan.patra@certainti.ai>
2024-04-29 21:59:03 +0530

    new changes

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,12 +1,16 @@
 const books = [ '1984', 'Brave New World', 'Farenheit 451' ];
 
-let foundBook;
+let foundBook:string | undefined;
 
 for (let book of books) {
     if (book === '1984') {
         foundBook = book;
+        console.log(foundBook.length);
+        
         break;
-    }
+    }  
 }
 
-console.log(foundBook);
+foundBook?.length
+
+console.log(foundBook?.length);
