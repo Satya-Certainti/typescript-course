fd79f1ce344152fbbcd9d3b41f8f288e8bdd3494
Satya-Certainti <satyanarayan.patra@certainti.ai>
2024-04-29 20:43:05 +0530

    saving modules as well

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,1 +1,20 @@
+console.log('typescript tutorial');
 
+let awesomeName: string = "ShakeAndBake";
+awesomeName = "Some thing else";
+
+awesomeName = awesomeName.toUpperCase();
+
+awesomeName = "123";
+
+console.log(awesomeName);
+
+let amount: number = 20;
+amount = 12 + 1;
+// amount = "Pants";
+console.log(amount);
+
+let isAwesome: boolean = true;
+
+isAwesome = false;
+isAwesome = "ShakeAndBake";
